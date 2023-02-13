f81f05e3fe0e420609d7704d7588d215201e3c00
jesue <jesue_gege@hotmail.com>
2023-02-13 19:26:47 -0300

    css 50%

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
+import '../styles/header.css';
 
 class Header extends Component {
   render() {
     const { name, email, score } = this.props;
     return (
-      <div>
+      <div className="header-content">
+        <p data-testid="header-player-name" className="name">{ name }</p>
+        <p data-testid="header-score" className="score">{ score }</p>
         <img
           src={ `https://www.gravatar.com/avatar/${md5(email).toString()}` }
           alt="teste"
           data-testid="header-profile-picture"
         />
-        <p data-testid="header-player-name">{ name }</p>
-        <p data-testid="header-score">{ score }</p>
       </div>
     );
   }
